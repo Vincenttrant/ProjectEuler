6ab9891d1db24e27432d25a3dcad5087bf6f6cb7
Vincent Tran <tranvincent73@gmail.com>
2023-10-31 23:31:10 -0500

    Create p9.py

---
+++ Python/p9.py
@@ -0,0 +1,10 @@
+def pythagorean():
+
+    for a in range(1, 1000):
+        for b in range(a, 1000 - a):
+            c = 1000 - a - b
+            if (a ** 2) + (b ** 2) == (c ** 2):
+                return a * b * c
+
+if __name__ == "__main__":
+    print(pythagorean())
